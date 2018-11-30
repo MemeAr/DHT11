2a71f0facf922d8e8788d22b9d690595b5c8c5c3
Unknown <amelie.ar98@gmail.com>
2018-11-30 10:20:13 +0100

    link css

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Temperature</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+	<h1>Température</h1>
 
-echo('Hello World')
-
-
-
-?>
+</body>
+</html>

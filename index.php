1106ac42ee44e05316ae81bc6cb423db0cb4aed6
Unknown <amelie.ar98@gmail.com>
2018-11-30 11:10:46 +0100

    Ouvrir .txt avec Json

--- index.php
+++ index.php
@@ -8,5 +8,22 @@
 <body>
 	<h1>Température</h1>
 
+	<?php
+		// function lecture($fichier) {
+		// 	$monFichier = file_get_contents('data.txt');
+		// 	var_dump(json_decode($monFichier));
+		// }
+
+		// $fichier = '/var/www/html/DHT11/data.txt';
+		// lecture($fichier);
+
+		$donnees = file_get_contents('data.txt');
+		$info = json_decode($donnees);
+	?>
+
+	<p>Il fait actuellement <?php echo($info -> temperature)?> et le taux d'humidité est de <?php echo($info -> humidite)?></p>
+
+
+
 </body>
 </html>
